e268e5a8242d5dccc31d5809153817bfcd7ba67e
Mohammed Hit <85726451+htmldev30@users.noreply.github.com>
2023-01-09 20:40:43 -0700

    Update FileInformation.js

--- frontend/src/shared/FileInformation.js
+++ frontend/src/shared/FileInformation.js
@@ -1,3 +1,5 @@
+// Code adapted from Expo FileSystem Documentation
+// https://docs.expo.dev/versions/latest/sdk/filesystem/
 import * as FileSystem from 'expo-file-system'
 
 export const getFileInfo = async (fileURI) => {
